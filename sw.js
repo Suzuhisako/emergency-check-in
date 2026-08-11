9366aabffcdfc32093a31187af5f84fddb5308f1
Suzuhisako <92011909+Suzuhisako@users.noreply.github.com>
2026-08-11 23:04:12 +0900

    Change icon file from SVG to PNG in service worker
    Updated icon file from 'icon.svg' to 'icon.png' in asset cache.

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon.svg",
+  "./icon.png",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 ];
