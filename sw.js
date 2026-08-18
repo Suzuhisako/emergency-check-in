293a62c123c38cffb1fc25c62765e27d7ab4b768
Suzuhisako <92011909+Suzuhisako@users.noreply.github.com>
2026-08-18 11:35:09 +0900

    Refactor service worker for clarity and maintainability

--- sw.js
+++ sw.js
@@ -5,7 +5,6 @@ const STATIC_ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './db.js',
   './icon-192.png',
   './icon-512.png'
   // 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
